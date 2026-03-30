f4221fdcecb8a19ca7ecd50744f6b6c03b6d7ff7
cuongpubgctb <cuongpubgctb@gmail.com>
2026-03-30 12:34:17 +0000

    hi

--- src/app/stock-items/stock-items.ts
+++ src/app/stock-items/stock-items.ts
@@ -7,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './stock-items.css',
 })
 export class StockItems implements OnInit{
+  public name!: string; // ":" là khai báo kiểu dữ liệu cho biến
+  public code!: string;
+  public price!: number;
+  public previousPrice!: number;
+  public positiveChange!: boolean;
+  public favorite!: boolean;
 
+  constructor(){
+
+  }
   ngOnInit(){
-    
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
+    this.positiveChange = this.price >= this.previousPrice;
+    this.favorite = false;
+
+    //nếu giá hiện tại price >= giá trước đó previus price
+    //thì positiveChange = true
+    //ngược lại positiveChange = false
+  }
+  addToFavorite(){
+    console.log('Add to favorite');
+    this.favorite = !this.favorite;
   }
 }
