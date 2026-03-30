edfa19f1d481a8a091faa805346778d9715f6587
cuongpubgctb <cuongpubgctb@gmail.com>
2026-03-30 14:24:46 +0000

    bài2

--- src/app/model/stock.ts
+++ src/app/model/stock.ts
@@ -2,12 +2,14 @@ export class Stock {
     favorite: boolean = false;
 
     constructor(
-        public name: string
+        public name: string // ":" là khai báo kiểu dữ liệu cho biến
         , public code: string
         , public price: number
-        , public previousPrice: number{}
-    )
+        , public previousPrice: number){}
     isPositiveChange(): boolean{
         return this.price >= this.previousPrice;
+            //nếu giá hiện tại price >= giá trước đó previus price
+            //thì positiveChange = true
+            //ngược lại positiveChange = false
     }
 }
--- src/app/stock-items/stock-items.ts
+++ src/app/stock-items/stock-items.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from '../model/stock';
 
 @Component({
   selector: 'app-stock-items',
@@ -7,30 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './stock-items.css',
 })
 export class StockItems implements OnInit{
-  public name!: string; // ":" là khai báo kiểu dữ liệu cho biến
-  public code!: string;
-  public price!: number;
-  public previousPrice!: number;
-  public positiveChange!: boolean;
-  public favorite!: boolean;
 
+  public stock!: Stock;
   constructor(){
 
   }
   ngOnInit(){
-    this.name = 'Test Stock Company';
-    this.code = 'TSC';
-    this.price = 85;
-    this.previousPrice = 80;
-    this.positiveChange = this.price >= this.previousPrice;
-    this.favorite = false;
-
-    //nếu giá hiện tại price >= giá trước đó previus price
-    //thì positiveChange = true
-    //ngược lại positiveChange = false
+    this.stock = new Stock('Apple', 'AAPL', 100, 80);
   }
   addToFavorite(){
     console.log('Add to favorite');
-    this.favorite = !this.favorite;
+    this.stock.favorite = !this.stock.favorite;
   }
 }
